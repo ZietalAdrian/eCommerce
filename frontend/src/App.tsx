92ef3fecde7127d7ded8260749a19b519060b0ba
Adrian Zietal <zietal.adrian@gmail.com>
2023-01-03 12:37:08 +0100

    add login and signup layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { FaShoppingBag } from "react-icons/fa";
+import axios from "axios";
 
 import Grid from "./components/Grid";
 import Home from "./components/Home";
 import Product from "./components/Product";
 import NotFound from "./components/NotFound";
 import ShoppingCart from "./components/ShoppingCart";
-import axios from "axios";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 type User = {
   id: string;
@@ -37,9 +39,17 @@ function App() {
   };
 
   return (
-    <div className="pb-32">
+    <div>
       <nav className="mb-10">
         <div className="h-10 flex justify-end items-center mx-8">
+          <ul className="flex pr-5 mr-8 gap-5 border-solid border-r-[1px] border-black">
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </ul>
           <button onClick={() => setShoppingCart((prev: boolean) => !prev)}>
             <FaShoppingBag className="text-xl" />
           </button>
@@ -67,6 +77,8 @@ function App() {
             <Route index element={<Grid getId={getId} />} />
             <Route path=":id" element={<Product getId={getId} />} />
           </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </main>
